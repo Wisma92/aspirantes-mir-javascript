87490611bba5eb57ba5553a7d48b53885f124e96
Rafael Mendoza <112434814+Wisma92@users.noreply.github.com>
2023-05-15 19:46:33 -0500

    Add files via upload

---
+++ js-3/object1.js
@@ -0,0 +1,29 @@
+let pedro = {
+    nombre: "Pedro Perez",
+    edad: 30,
+    activo: true,
+    hobbies: ["programar", "squash", "piano"]
+}
+
+console.log(pedro.edad);
+
+pedro.estatura = 1.8;
+
+delete pedro.activo;
+
+for (let llave in pedro) {
+    if (pedro.hasOwnProperty(llave)) {
+        console.log(pedro[llave])
+    }
+}
+
+    console.log("Nombre: " + pedro.nombre);
+    console.log("Edad: " + pedro.edad);
+    console.log("Estatura: " + pedro.estatura);
+    console.log("Hobbies: " + pedro.hobbies.join(", "));
+
+    saluda: function () {
+    console.log("Hola, me llamo " + pedro.nombre);
+}
+
+saluda();
